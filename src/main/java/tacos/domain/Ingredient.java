3682f642fdfd16e3b1de7a096742abfbb4b5df73
Avocado <ujubongbong@gmail.com>
2025-04-03 21:39:12 +0900

    :bug: fix: 재료 클래스 Type의 import 제거

--- src/main/java/tacos/domain/Ingredient.java
+++ src/main/java/tacos/domain/Ingredient.java
@@ -1,13 +1,11 @@
 package tacos.domain;
-
-import java.lang.reflect.Type;
-
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
 public class Ingredient {
+
 	private final String id;
 	private final String name;
 	private final Type type;
