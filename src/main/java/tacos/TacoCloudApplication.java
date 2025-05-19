de3f97c34b73efbe4849c9e3e0b4107dafa5b825
Avocado <ujubongbong@gmail.com>
2025-05-20 00:44:44 +0900

    :sparkles: feat: 2.4 뷰 컨트롤러로 작업하기

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -2,12 +2,19 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TacoCloudApplication {
+public class TacoCloudApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+	// 구성 클래스 간단하게 유지하기 위함
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 }
