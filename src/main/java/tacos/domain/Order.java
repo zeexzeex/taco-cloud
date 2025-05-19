2e5be93c36fda9dbeca803551786c10c9f362b1a
Avocado <ujubongbong@gmail.com>
2025-05-20 00:17:34 +0900

    :sparkles: feat: 2.3.1 유효성 검사 규칙 선언하기

--- src/main/java/tacos/domain/Order.java
+++ src/main/java/tacos/domain/Order.java
@@ -1,14 +1,37 @@
 package tacos.domain;
 
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class Order {
+
+	@NotBlank(message="Name is required")
 	private String deliveryName;
+
+	@NotBlank(message="Street is required")
 	private String deliveryStreet;
+
+	@NotBlank(message="City is required")
 	private String deliveryCity;
+
+	@NotBlank(message="State is required")
+	private String deliveryState;
+
+	@NotBlank(message="Zip code is required")
 	private String deliveryZip;
-	private String ccNumber;	// credit card
+
+	@CreditCardNumber(message="Not a valid credit card number")
+	private String ccNumber;
+
+	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+		message="Must be formatted MM/YY")
 	private String ccExpiration;
-	private String ccCvv;
+
+	@Digits(integer=3, fraction=0, message="Invalid CVV")
+	private String ccCVV;
 }
