5009b7249dec330df8380fe15046c47d5f7fe916
Avocado <ujubongbong@gmail.com>
2025-05-20 23:14:48 +0900

    :bug: fix: Ingredient의 기본키 설정이 없어 외래키 조건 만족하지 못하는 문제 해결

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 create table if not exists Ingredient (
   id varchar(4) not null,
   name varchar(25) not null,
-  type varchar(10) not null
+  type varchar(10) not null,
+  primary key (id)
 );
 
 create table if not exists Taco (
