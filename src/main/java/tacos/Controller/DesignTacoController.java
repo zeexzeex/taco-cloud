e3b5066219777ccf052ee092f94669bd20415c84
Avocado <ujubongbong@gmail.com>
2025-05-20 22:23:07 +0900

    :sparkles: feat: 3.12 타코 디자인 저장하고 주문과 연결시키기

--- src/main/java/tacos/Controller/DesignTacoController.java
+++ src/main/java/tacos/Controller/DesignTacoController.java
@@ -11,17 +11,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.validation.Valid;
 import tacos.data.IngredientRepository;
 import tacos.data.TacoRepository;
 import tacos.domain.Ingredient;
+import tacos.domain.Order;
 import tacos.domain.Taco;
 
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order") // 다수 요청 처리 (세션에 저장)
 public class DesignTacoController {
 	private final IngredientRepository ingredientRepository;
 	private TacoRepository tacoRepository;
@@ -30,6 +34,16 @@ public class DesignTacoController {
 	public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository tacoRepository) {
 		this.ingredientRepository = ingredientRepository;
 		this.tacoRepository = tacoRepository;
+	}
+
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
+	}
+
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
 	}
 
 	// DB에서 조회해서 가져오기
@@ -55,14 +69,16 @@ public class DesignTacoController {
 			.collect(Collectors.toList());
 	}
 
-	// 타코 유효성 검사
+	// 타코 유효성 검사, 타코 저장
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
 		if(errors.hasErrors()) {
 			return "design";
 		}
 
-		// log.info("Processing Design: " + design);
+		Taco saved = tacoRepository.save(design);
+		order.addDesign(saved);
+
 		return "redirect:/orders/current";
 	}
 }
--- src/main/java/tacos/domain/Order.java
+++ src/main/java/tacos/domain/Order.java
@@ -1,6 +1,8 @@
 package tacos.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 
@@ -38,4 +40,10 @@ public class Order {
 
 	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
+
+	private List<Taco> tacos = new ArrayList<>();
+
+	public void addDesign(Taco design) {
+		this.tacos.add(design);
+	}
 }
